95cf1be264783a640514c7297c77a77c38519006
fongas01 <nando.fongas@gmail.com>
2023-04-07 05:04:21 -0300

    Acertando as arestas

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,2 @@
 document.getElementById('paragrafoInvisivel').innerHTML = 'FULL APPEND SYSTEM';
+//document.querySelector('.fotoMap').innerHTML = "<img src='img/map-Africa.png' width='100' border='1px solid' >";
