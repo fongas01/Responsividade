eefdead10fcabe6a3a1df01ed07648a31081e39f
fongas01 <nando.fongas@gmail.com>
2023-04-18 22:03:03 -0300

    Melhorando o Footer

---
+++ menu-hamburger/menu-burger.js
@@ -0,0 +1,11 @@
+//let menu = document.querySelector('menu-burger');
+
+function clickMenu(){
+    // FAZ OS ITENS APARECEREM E SUMIREM
+    if (itens.style.display == 'block'){
+        itens.style.display = "none"
+    } else{
+        itens.style.display = "block"
+    }
+   
+}
